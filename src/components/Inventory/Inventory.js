0cfe2027c28b05279ade47dcd90287a6bf2ad25e
কচুখালী সঃপ্রাঃবিঃ <you@example.com>
2021-10-02 21:51:10 -0700

    add some text

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h1>this is Inventory</h1>
+            <h1>this is Inventory pages</h1>
         </div>
     );
 };
